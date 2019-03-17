9477915612ed7f167f74235e57af77c9d198ac6a
aocattleya <39142850+aocattleya@users.noreply.github.com>
2019-03-17 23:22:56 +0900

    ページすべてを取得する

---
+++ html_all.py
@@ -0,0 +1,12 @@
+﻿# ライブラリの読み込み
+from bs4 import BeautifulSoup
+import urllib.request
+
+# URL
+url = "https://www.google.com/"
+# urlにアクセス 
+html = urllib.request.urlopen(url)
+# htmlをBeautifulSoupで扱う
+soup = BeautifulSoup(html, "html.parser")
+# 出力
+print(soup)
