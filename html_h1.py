099d78ef6d24732262153aae2e69469e8d72fa85
aocattleya <aocattleya@yahoo.co.jp>
2019-03-24 03:21:53 +0900

    要素1つを取得する

---
+++ html_h1.py
@@ -0,0 +1,18 @@
+﻿# ライブラリの読み込み
+from bs4 import BeautifulSoup
+import urllib.request
+
+# URL
+url = "https://www.yahoo.co.jp/"
+
+# URLにアクセス 
+html = urllib.request.urlopen(url)
+
+# HTMLをBeautifulSoupで扱う
+soup = BeautifulSoup(html, "html.parser")
+
+# 出力
+print(soup.find("h1"))
+
+# .get_text()を付けて抽出
+print(soup.find("h1").get_text())
